8d6b43691fb16fb8ae39107a79cfd9094653dc57
Alexander Biryukov <sanya.rnd@gmail.com>
2025-05-04 03:36:40 +0300

    some fixes

--- frontend/src/main/typescript/backend/dto/songTypeStats.ts
+++ frontend/src/main/typescript/backend/dto/songTypeStats.ts
@@ -1,6 +1,6 @@
 import { SongType } from "@/backend/dto/enumeration";
 
-export type SongTypeStats = { [key: SongType]: SongTypeStatsRecord };
+export type SongTypeStats = { [key in SongType]: SongTypeStatsRecord };
 
 export interface SongTypeStatsRecord {
   type: SongType;
